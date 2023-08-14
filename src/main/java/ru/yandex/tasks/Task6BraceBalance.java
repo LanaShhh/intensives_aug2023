1d6591df98049c0c8fa59a55e984dc416123b235
felix <tigroff.mikhail@yandex.ru>
2023-08-14 20:21:25 +0300

    task 6

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,5 +1,7 @@
 package ru.yandex.tasks;
 
+import java.util.Stack;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
@@ -7,7 +9,22 @@ public class Task6BraceBalance {
          * Выход: true/false, является ли строка ПСП
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < sequence.length(); i++) {
+            if (sequence.charAt(i) == '(' || sequence.charAt(i) == '[' || sequence.charAt(i) == '{') {
+                stack.push(sequence.charAt(i));
+            }
+            else if (sequence.charAt(i) == ')') {
+                if (stack.pop() != '(') return false;
+            }
+            else if (sequence.charAt(i) == ']') {
+                if (stack.pop() != '[') return false;
+            }
+            else if (sequence.charAt(i) == '}') {
+                if (stack.pop() != '{') return false;
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
@@ -16,5 +33,9 @@ public class Task6BraceBalance {
 
         assert checkBalance(test1);
         assert !checkBalance(test2);
+    }
+
+    public static void main(String[] args) {
+        selfCheck();
     }
 }
