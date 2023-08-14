a4bb62c1a1f524490df936e01202db789c1c76d7
felix <tigroff.mikhail@yandex.ru>
2023-08-14 19:07:47 +0300

    task 2

--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -4,13 +4,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Task2SelectionSort {
+    public static void main(String[] args) {
+        selfCheck();
+    }
+
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
         /*
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: отсортированный (сортировкой выбором!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size(); i++) {
+            int min_i = i;
+            int min = numbers.get(i);
+            for (int j = i; j < numbers.size(); j++) {
+                if (numbers.get(j) < min) {
+                    min_i = j;
+                    min = numbers.get(j);
+                }
+            }
+            int temp = numbers.get(i);
+            numbers.set(i, min);
+            numbers.set(min_i, temp);
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
