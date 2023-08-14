07575541a4fc1d908984ea067db14aaaa01b74c5
LanaShhh <84940283+LanaShhh@users.noreply.github.com>
2023-08-14 17:11:26 +0300

    Update Node.java

--- src/main/java/ru/yandex/tasks/Node.java
+++ src/main/java/ru/yandex/tasks/Node.java
@@ -5,7 +5,6 @@ public class Node {
      * Класс для односвязного списка
      * value - значение, -10^5 <= value <= 10^5
      * next - следующий элемент
-     * Можно менять под свои нужды, value и next не трогаем :)
      */
     public int value;
     public Node next;
