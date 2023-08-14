367755e057d7d86bae2486db386adc7cec37c11b
undina <foxundina@gmail.com>
2023-08-14 18:29:24 +0300

    bubble

--- src/main/java/ru/yandex/tasks/Task1BubbleSort.java
+++ src/main/java/ru/yandex/tasks/Task1BubbleSort.java
@@ -5,12 +5,26 @@ import java.util.Arrays;
 
 public class Task1BubbleSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < numbers.size() - i - 1; j++) {
+                if (numbers.get(j) > numbers.get(j + 1)) {
+                    int temp = numbers.get(j);
+                    numbers.set(j, numbers.get(j + 1));
+                    numbers.set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+        System.out.println(numbers);
         /*
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: отсортированный (сортировкой пузырьком!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        return numbers;
     }
 
     public static void selfCheck() {
@@ -18,5 +32,10 @@ public class Task1BubbleSort {
         ArrayList<Integer> output = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
         assert output.equals(sort(input));
+    }
+
+    public static void main(String[] args) {
+        sort(new ArrayList<>(Arrays.asList(3, 4, 5, 2, 1)));
+        selfCheck();
     }
 }
