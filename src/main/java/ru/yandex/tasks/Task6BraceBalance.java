d8b733b14b2645f8ce7eec3d18026716e5d9e984
aandilyin <AAndIlyin@sberbank.ru>
2023-08-14 20:10:10 +0300

    Task06 - brackets

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,20 +1,42 @@
 package ru.yandex.tasks;
 
+import java.util.Stack;
+
 public class Task6BraceBalance {
-    public static boolean checkBalance(String sequence) {
-        /*
-         * sequence - последовательность скобок []{}() длины до 10^5
-         * Выход: true/false, является ли строка ПСП
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
-    }
 
-    public static void selfCheck() {
-        String test1 = "[({})]{[]}";
-        String test2 = "{({})}{";
+	public static Character getPair(Character bracket) {
+		return switch (bracket) {
+			case '}' -> '{';
+			case ')' -> '(';
+			case ']' -> '[';
+			default -> '-';
+		};
+	}
 
-        assert checkBalance(test1);
-        assert !checkBalance(test2);
-    }
+
+	public static boolean checkBalance(String sequence) {
+		/*
+		 * sequence - последовательность скобок []{}() длины до 10^5
+		 * Выход: true/false, является ли строка ПСП
+		 */
+		// (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+		Stack<Character> stack = new Stack<>();
+		for (char ch : sequence.toCharArray()) {
+			if (ch == '[' || ch == '(' || ch == '{') {
+				stack.push(ch);
+			} else if (stack.pop() != getPair(ch)) {
+				return false;
+			}
+		}
+
+		return stack.empty();
+	}
+
+	public static void selfCheck() {
+		String test1 = "[({})]{[]}";
+		String test2 = "{({})}{";
+
+		assert checkBalance(test1);
+		assert !checkBalance(test2);
+	}
 }
