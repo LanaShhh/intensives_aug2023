5432e7de6f88f5060fceca9b3abd5e2bb76afa65
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2023-08-14 20:51:54 +0300

    seventh done

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -3,6 +3,7 @@ package ru.yandex;
 import ru.yandex.tasks.Task1BubbleSort;
 import ru.yandex.tasks.Task2SelectionSort;
 import ru.yandex.tasks.Task4DFS;
+import ru.yandex.tasks.Task7PrimeNumbers;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,6 @@ public class Main {
         Task2SelectionSort.selfCheck();
         Task4DFS.selfCheck();
         Task4DFS.selfCheck();
+        Task7PrimeNumbers.selfCheck();
     }
 }
--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -1,6 +1,8 @@
 package ru.yandex.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task7PrimeNumbers {
     public static int[] findPrimes (int N) {
@@ -9,7 +11,25 @@ public class Task7PrimeNumbers {
          * Выход: отсортированный массив всех простых чисел от 2 до N
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 2; i < N + 1; i++) {
+            if (isPrime[i]) resultList.add(i);
+        }
+
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void selfCheck() {
