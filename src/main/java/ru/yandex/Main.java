72899a728fa413190ee88dfc4cb2ace1f461953b
elyutinmaks <elyutin15@gmail.com>
2023-08-14 18:31:27 +0300

    solve 1 task

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -1,7 +1,9 @@
 package ru.yandex;
 
+import ru.yandex.tasks.Task1BubbleSort;
+
 public class Main {
     public static void main(String[] args) {
-
+        Task1BubbleSort.selfCheck();
     }
 }
--- src/main/java/ru/yandex/tasks/Task1BubbleSort.java
+++ src/main/java/ru/yandex/tasks/Task1BubbleSort.java
@@ -2,6 +2,8 @@ package ru.yandex.tasks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Task1BubbleSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
@@ -10,13 +12,21 @@ public class Task1BubbleSort {
          * Выход: отсортированный (сортировкой пузырьком!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        int n = numbers.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (numbers.get(j) > numbers.get(j + 1)) {
+                    Collections.swap(numbers, j, j + 1);
+                }
+            }
+        }
+        return numbers;
+
     }
 
     public static void selfCheck() {
         ArrayList<Integer> input = new ArrayList<>(Arrays.asList(3, 4, 5, 2, 1));
         ArrayList<Integer> output = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-
         assert output.equals(sort(input));
     }
 }
