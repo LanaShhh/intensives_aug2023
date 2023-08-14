d5715dbaab368b33781938d10c126bd49d095285
wlokas <maksim.sadowscky@yandex.ru>
2023-08-14 18:51:13 +0300

    complite

--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -4,13 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Task2SelectionSort {
+
+    public static void main(String[] args) {
+        selfCheck();
+    }
+
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
         /*
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: отсортированный (сортировкой выбором!) numbers
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int maxSize = numbers.size(); maxSize > 0; maxSize--) {
+            int maxIndex = 0;
+            for (int i = 0; i < maxSize; i++) {
+                if (numbers.get(maxIndex) < numbers.get(i)) {
+                    maxIndex = i;
+                }
+            }
+            Integer buffer = numbers.get(maxSize - 1);
+            numbers.set(maxSize - 1, numbers.get(maxIndex));
+            numbers.set(maxIndex, buffer);
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
