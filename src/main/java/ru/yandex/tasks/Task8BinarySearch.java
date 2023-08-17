9d8b8a2e2477c923df16f65ae9554cf2a862e78d
LanaShhh <84940283+LanaShhh@users.noreply.github.com>
2023-08-17 14:08:40 +0300

    Update Task8BinarySearch.java

--- src/main/java/ru/yandex/tasks/Task8BinarySearch.java
+++ src/main/java/ru/yandex/tasks/Task8BinarySearch.java
@@ -4,7 +4,7 @@ public class Task8BinarySearch {
     public static int find(int number, int[] sortedList) {
         /*
          * number: целое число, -10^5 <= number <= 10^5
-         * numbers: массив целых чисел, 0 <= numbers.length <= 10^5 (числа не повторяются)
+         * numbers: массив целых чисел, 0 <= numbers.length <= 10^5
          * Выход: i, где numbers[i] = number, -1, если такого i нет
          * Если таких i несколько, вывести наибольший
          */
