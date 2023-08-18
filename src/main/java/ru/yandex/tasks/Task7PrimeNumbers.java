e526862d063fa6d1b527f7d1ac90918a1cb68f2f
Sasha <sasha.g27204@gmail.com>
2023-08-18 17:32:12 +0300

    eratosthenes

--- src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
+++ src/main/java/ru/yandex/tasks/Task7PrimeNumbers.java
@@ -11,7 +11,25 @@ public class Task7PrimeNumbers {
          * Выход: отсортированный массив всех простых чисел от 2 до N
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        boolean[] isSimple = new boolean[N + 2];
+        Arrays.fill(isSimple, true);
+        int resLen = 0;
+        for (int i = 2; i  < N + 2; i++) {
+            if (isSimple[i]) {
+                resLen++;
+                for (int j = 2; i * j < N + 2; j++) {
+                    isSimple[j * i] = false;
+                }
+            }
+        }
+        int[] res = new int[resLen];
+        int pointer = 0;
+        for (int i = 2; i < N + 2; i++) {
+            if (isSimple[i]) {
+                res[pointer++] = i;
+            }
+        }
+        return res;
     }
 
     public static void selfCheck() {
